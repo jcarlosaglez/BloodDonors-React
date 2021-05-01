1ed30ef2f4e7b8315f334ba708fbbaeaa4a6e2d6
sebasgar13 <sebastian.garciap@alumno.buap.mx>
2021-05-01 12:46:53 -0500

    eliminado card circle y modificando car con Material UI

--- src/components/TranspSec.js
+++ src/components/TranspSec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CardCir from "./CardCir";
+import Card from "./Card";
 
 function TranspSec(props) {
     const imgUrl = props.imagen;
@@ -14,12 +14,12 @@ function TranspSec(props) {
 
     const data = props.data;
     let roots = data.map((product, index, array) => {
-        return <CardCir datos={product} key={index} />;})
+        return <Card datos={product} key={index} />;})
     return (
-        <section className="my-40 h-auto lg:h-screen flex flex-col justify-center aling-around text-center" style={divStyle}>
-            <h2 className="my-10"> HOla </h2>
+        <section className="" style={divStyle}>
+            <h2 className=""> HOla </h2>
             <h3> Que tal</h3>
-            <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="">
                 {roots}   
             </div>
         </section>
