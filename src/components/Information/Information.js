0694ddb9abd9b9a3a50f4904158a23ad5b0d6a39
sebasgar13 <sebastian.garciap@alumno.buap.mx>
2021-06-30 16:37:04 -0500

    Cambios esteticos

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -5,6 +5,8 @@ import ListOrden from "./ListOrden";
 import requirements from "../../assets/data/requirements";
 import recommen from "../../assets/data/recommendations";
 
+import '../../css/Information/HomeInformation.css'
+
 function Users() {
     const padding = {
       padding: "10px 10% 50px 10%"
@@ -17,13 +19,15 @@ function Users() {
         buttonMos="none"
         image="https://images.unsplash.com/photo-1519032465794-2da0ceef0b63?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
         />
-        <SectionOfTitle title="Los requisitos mínimos para donar sangre"/>
-        <div style={padding}>
-          <ListOrden data={requirements} />
-        </div>
-        <SectionOfTitle title="Recomendaciones"/>
-        <div style={padding}>
-          <ListOrden data={recommen} />
+        <div className="padding">
+          <SectionOfTitle title="Los requisitos mínimos para donar sangre"/>
+          <div style={padding}>
+            <ListOrden data={requirements} />
+          </div>
+          <SectionOfTitle title="Recomendaciones"/>
+          <div style={padding}>
+            <ListOrden data={recommen} />
+          </div>
         </div>
       </>
     );
